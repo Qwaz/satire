74922dac8304b2f123914d27a7baf28e669d15a5
Yechan Bae <yechan@gatech.edu>
2021-07-11 19:22:57 -0400

    Add session abstraction

--- src/solver/cdcl/conflict.rs
+++ src/solver/cdcl/conflict.rs
@@ -1,5 +1,3 @@
-use std::mem::replace;
-
 use crate::formula::{Clause, Literal, Variable};
 
 pub trait ConflictDataProvider {
@@ -19,10 +17,57 @@ pub struct ConflictAnalyzer {
     seen: Vec<bool>,
     /// A queue that records seen variables.
     seen_queue: Vec<Variable>,
+}
+
+struct Session<'inner, 'solver, P> {
+    inner: &'inner mut ConflictAnalyzer,
+    current_level: usize,
+    data_provider: &'solver P,
     /// A clause to learn
     recorded: Vec<Literal>,
     /// Unresolved variables on the current level
     unresolved_on_current_level: usize,
+}
+
+impl<'inner, 'solver, P> Session<'inner, 'solver, P>
+where
+    P: ConflictDataProvider,
+{
+    pub fn new(
+        inner: &'inner mut ConflictAnalyzer,
+        data_provider: &'solver P,
+        current_level: usize,
+    ) -> Self {
+        Session {
+            inner,
+            current_level,
+            data_provider,
+            recorded: Vec::new(),
+            unresolved_on_current_level: 0,
+        }
+    }
+
+    pub fn add_clause(&mut self, clause: &Clause) {
+        for literal in clause.iter() {
+            if self.inner.mark_if_unseen(literal.variable()) {
+                let literal_level = self.data_provider.level(literal.variable());
+                if literal_level == self.current_level {
+                    self.unresolved_on_current_level += 1;
+                } else if literal_level != 0 {
+                    self.recorded.push(literal);
+                }
+            }
+        }
+    }
+
+    pub fn seen(&self, variable: Variable) -> bool {
+        self.inner.seen[variable.index()]
+    }
+
+    pub fn finish(self) -> Clause {
+        self.inner.clear();
+        Clause::new(self.recorded)
+    }
 }
 
 impl ConflictAnalyzer {
@@ -30,20 +75,14 @@ impl ConflictAnalyzer {
         ConflictAnalyzer {
             seen: vec![false; num_variables],
             seen_queue: Vec::new(),
-            recorded: Vec::new(),
-            unresolved_on_current_level: 0,
         }
     }
 
-    fn finalize(&mut self) -> Clause {
+    fn clear(&mut self) {
         for &var in &self.seen_queue {
             self.seen[var.index()] = false;
         }
         self.seen_queue.clear();
-        let recorded = replace(&mut self.recorded, Default::default());
-        self.unresolved_on_current_level = 0;
-
-        Clause::new(recorded)
     }
 
     /// Mark the variable, return true if the variable is previously unseen.
@@ -57,22 +96,6 @@ impl ConflictAnalyzer {
         }
     }
 
-    fn add_clause<P>(&mut self, current_level: usize, data_provider: &P, clause: &Clause)
-    where
-        P: ConflictDataProvider,
-    {
-        for literal in clause.iter() {
-            if self.mark_if_unseen(literal.variable()) {
-                let literal_level = data_provider.level(literal.variable());
-                if literal_level == current_level {
-                    self.unresolved_on_current_level += 1;
-                } else if literal_level != 0 {
-                    self.recorded.push(literal);
-                }
-            }
-        }
-    }
-
     pub fn analyze<P>(
         &mut self,
         data_provider: &P,
@@ -83,23 +106,25 @@ impl ConflictAnalyzer {
     where
         P: ConflictDataProvider,
     {
-        self.add_clause(current_level, data_provider, conflicting_clause);
+        let mut session = Session::new(self, data_provider, current_level);
+        session.add_clause(conflicting_clause);
 
         for literal in literals.iter().rev().copied() {
             let variable = literal.variable();
-            if self.seen[variable.index()] {
-                self.unresolved_on_current_level -= 1;
-                if self.unresolved_on_current_level == 0 {
+            if session.seen(variable) {
+                session.unresolved_on_current_level -= 1;
+                if session.unresolved_on_current_level == 0 {
                     // First UIP reached
-                    self.recorded
+                    session
+                        .recorded
                         .push(Literal::new(variable, !data_provider.value(variable)));
 
-                    return self.finalize();
+                    return session.finish();
                 }
 
                 // If this was not UIP, mark its antecedents
                 let antecedents = data_provider.antecedents(literal.variable()).unwrap();
-                self.add_clause(current_level, data_provider, antecedents);
+                session.add_clause(antecedents);
             }
         }
 
